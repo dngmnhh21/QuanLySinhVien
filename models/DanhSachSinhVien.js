414252baa41b76d835cc791b6fbfebc1f6be0737
Hieu <minhhieu2120@gmail.com>
2023-12-02 20:56:05 +0700

    2/12

---
+++ models/DanhSachSinhVien.js
@@ -0,0 +1,15 @@
+function DanhSachSinhVien (){
+    this.DSSV = [];
+    this.ThemSinhVien = function(svThem){
+        this.DSSV.push(svThem);
+    }
+    this.XoaSinhVien = function(listSVXoa){
+
+    }
+    this.SuaSinhVien = function(svCapNhat){
+
+    }
+    this.TimKiemSinhVien = function(keyWWord){
+
+    }
+}
