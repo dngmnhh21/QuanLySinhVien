3ae110f41cf10622b1270403ca70ed8dbe996d25
Hieu <minhhieu2120@gmail.com>
2023-12-04 22:03:57 +0700

    4/12

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 var danhSachSinhVien = new DanhSachSinhVien();
+
+getStorage();
+
 var validate = new Validation();
+
+//bổ sung thuộc tính
+SinhVien.prototype.DiemToan = '';
+SinhVien.prototype.DiemLy = '';
+SinhVien.prototype.DiemHoa = '';
+SinhVien.prototype.DTB = '';
+SinhVien.prototype.Loai = '';
+
+//thêm phương thức
+SinhVien.prototype.TinhDTB = function(){
+    this.DTB = (Number(this.DiemToan) + Number(this.DiemLy)+ Number(this.DiemHoa))/3;
+}
+SinhVien.prototype.XepLoai =  function(){
+    if(this.DTB <= 10 && this.DTB >= 8){
+        this.Loai = "Xếp loại giỏi";
+    }else if(this.DTB < 8 && this.DTB >= 6.5){
+        this.Loai = "Xếp loại khá";
+    }else if(this.DTB < 6.5 && this.DTB >= 5){
+        this.Loai = "Xếp loại trung bình";
+    }else{
+        this.Loai = "Xếp loại yếu";
+    }
+
+}
+
+
 function DomID(id){
     var element = document.getElementById(id);
     return element;
@@ -42,6 +71,11 @@ function ThemSinhVien(){
     }
     //thêm sinh viên 
     var sinhvien = new SinhVien(masv,hoten,email,sdt,cmnd);
+    sinhvien.DiemToan = DomID("toan").value;
+    sinhvien.DiemLy = DomID("ly").value;
+    sinhvien.DiemHoa = DomID("hoa").value;
+    sinhvien.TinhDTB();
+    sinhvien.XepLoai();
     danhSachSinhVien.ThemSinhVien(sinhvien);
     CapNhatDanhSachSV(danhSachSinhVien);
     console.log(danhSachSinhVien)
@@ -66,20 +100,39 @@ function CapNhatDanhSachSV (DanhSachSinhVien){
     for(var i = 0; i < DanhSachSinhVien.DSSV.length; i++){
         var sv = danhSachSinhVien.DSSV[i];
         var trSinhVien = document.createElement("tr");
+        trSinhVien.id = sv.MaSV;
+        trSinhVien.className = "trSinhVien";
+        trSinhVien.setAttribute("onclick","ChinhSuaSinhvien('"+sv.MaSV+"')");
         //tạo các thẻ td và filter dữ liệu sinh viên thứ [i] vào
         var tdCheckBox = document.createElement("td");
+        var ckbMaSinhVien =document.createElement("input");
+        console.log(ckbMaSinhVien);
+        ckbMaSinhVien.setAttribute("class", "ckbMaSV");
+        ckbMaSinhVien.setAttribute("type", "checkbox");
+        ckbMaSinhVien.setAttribute("value",sv.MaSV);
+        tdCheckBox.appendChild(ckbMaSinhVien);
+
         var tdMaSV = TaoTheTD("MaSV", sv.MaSV);
         var tdHoTen = TaoTheTD("HoTen", sv.HoTen);
         var tdCMND = TaoTheTD("CMND", sv.CMND);
         var tdEmail = TaoTheTD("Email", sv.Email);
         var tdSoDT = TaoTheTD("SoDT", sv.SoDT);
+
+        //tạo td DTB và xếp loại
+        var tdDTB = TaoTheTD("DTB", sv.DTB);
+        var tdXepLoai = TaoTheTD("XepLoai", sv.Loai);
+
+
         //append các td vào tr
+        
         trSinhVien.appendChild(tdCheckBox);
         trSinhVien.appendChild(tdMaSV);
         trSinhVien.appendChild(tdHoTen);
         trSinhVien.appendChild(tdCMND);
         trSinhVien.appendChild(tdEmail);
         trSinhVien.appendChild(tdSoDT);
+        trSinhVien.appendChild(tdDTB);
+        trSinhVien.appendChild(tdXepLoai);
         //append các tr vào tbodySinhVien
         listTableSV.appendChild(trSinhVien);
         
@@ -107,4 +160,83 @@ function getStorage(){
     var mangDSSV = JSON.parse(jsonDanhSachSinhVien);
     danhSachSinhVien.DSSV = mangDSSV;
     CapNhatDanhSachSV(danhSachSinhVien);
+}
+
+//xoa sinh vien
+function XoaSinhVien(){
+    //mang checkbox
+    var listMaSV = document.getElementsByClassName("ckbMaSV");
+    //mang ma sv dc chon 
+    var listMaSVDuocChon = [];
+    for(i =0 ; i<listMaSV.length;i++){
+        console.log(listMaSV[i]);
+        if(listMaSV[i].checked){ // kiem tra phan tu checkbox co dc chon hay chua
+            listMaSVDuocChon.push(listMaSV[i].value);
+        }
+    }
+    danhSachSinhVien.XoaSinhVien(listMaSVDuocChon);
+    CapNhatDanhSachSV(danhSachSinhVien);
+}
+
+
+function TimKiemSinhVien(){
+    var tukhoa = DomID("tukhoa").value;
+    var listDSSVTimKiem = danhSachSinhVien.TimKiemSinhVien(tukhoa);
+    CapNhatDanhSachSV(listDSSVTimKiem);
+
+}
+
+function ChinhSuaSinhvien(masv){
+    var sinhvien = danhSachSinhVien.TimSVTheoMa(masv);
+    if(sinhvien != null){
+        DomID("masv").value = sinhvien.MaSV;
+        DomID("hoten").value = sinhvien.HoTen;
+        DomID("cmnd").value = sinhvien.CMND;
+        DomID("email").value = sinhvien.Email;
+        DomID("sdt").value = sinhvien.SoDT;
+    
+    }
+
+}
+
+function LuuThongTin(){
+    var masv = DomID("masv").value;
+    var hoten = DomID("hoten").value;
+    var cmnd = DomID("cmnd").value;
+    var email = DomID("email").value;
+    var sdt = DomID("sdt").value;
+    var loi = 0;
+    //kiểm tra validate
+    if(KiemTraDauVaoRong("masv",masv) == true){
+        loi++;
+    }
+    if(KiemTraDauVaoRong("hoten",hoten) == true){
+        loi++;
+    }
+    if(KiemTraDauVaoRong("cmnd",cmnd) == true){
+        loi++;
+    }
+    
+    if(validate.KiemTraEmail(email)){
+        document.getElementById("email").style.borderColor = "green";  
+    }else{
+        document.getElementById("email").style.borderColor = "red";  
+        loi++;
+    }
+    
+    if(validate.KiemTraSDT(sdt)){
+        document.getElementById("sdt").style.borderColor = "green";  
+    }else{
+        document.getElementById("sdt").style.borderColor = "red";  
+        loi++;
+    }
+    if(loi != 0){
+        return;
+    }
+    //thêm sinh viên 
+    var sinhvien = new SinhVien(masv,hoten,email,sdt,cmnd);
+    danhSachSinhVien.SuaSinhVien(sinhvien);
+    CapNhatDanhSachSV(danhSachSinhVien);
+    
+
 }
--- models/DanhSachSinhVien.js
+++ models/DanhSachSinhVien.js
@@ -4,12 +4,47 @@ function DanhSachSinhVien (){
         this.DSSV.push(svThem);
     }
     this.XoaSinhVien = function(listSVXoa){
+        for(var i=0;i<listSVXoa.length;i++){
+            for(var j=0; j<this.DSSV.length;j++){
+                var sinhvien = this.DSSV[j];
+                if(listSVXoa[i] == sinhvien.MaSV){
+                    this.DSSV.splice(j,1);
+                }
+            }
+        }
 
     }
     this.SuaSinhVien = function(svCapNhat){
+        for(var i = 0 ; i< this.DSSV.length; i++){
+            var svUpdate = this.DSSV[i];
+            if(svCapNhat.MaSV == svUpdate.MaSV){
+                svUpdate.HoTen = svCapNhat.HoTen;
+                svUpdate.Email = svCapNhat.Email;
+                svUpdate.CMND = svCapNhat.CMND;
+                svUpdate.SoDT = svCapNhat.SoDT;
+                
+            }
+        }
 
     }
-    this.TimKiemSinhVien = function(keyWWord){
+    this.TimKiemSinhVien = function(tuKhoa){
+        var listKetQuaTimKiem = new DanhSachSinhVien();
+        for(var i = 0 ; i<this.DSSV.length; i++){
+            var sinhvien = this.DSSV[i];
+            if(sinhvien.HoTen.toLowerCase().trim().search(tuKhoa.toLowerCase().trim()) != -1){
+                listKetQuaTimKiem.ThemSinhVien(sinhvien);
+            }
+        }
+        return listKetQuaTimKiem;
 
+    }
+    this.TimSVTheoMa = function(masv){
+        for(var i = 0 ; i< this.DSSV.length; i++){
+            var sv = this.DSSV[i];
+            if(sv.MaSV == masv){
+                return sv;
+            }
+        }
+        return null;
     }
 }
